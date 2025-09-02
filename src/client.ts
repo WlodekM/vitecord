db6815f4a3fb53acf3b6e058eabac6414afd003f
WlodekM <wlodzio.maly@gmail.com>
2025-09-02 10:51:10 +0300

    juikyfdserfdgfhgjhk

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,8 @@ if (!process.env)
 	process.env = {}
 const { Client, Constants } = await import("discord.js-selfbot-v13");
 const client = new Client();
+//TODO: probably make a proxy for gateway
+console.log(client.ws.gateway)
 client.on(Constants.Events.DEBUG, console.log)
 // client.on(Constants.Events.DEBUG, console.log)
 export default client;
